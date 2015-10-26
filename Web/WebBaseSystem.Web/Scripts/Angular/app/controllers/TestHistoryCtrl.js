06de5b3a1aaf3d9dec2925af6a0e37e2da229ade
bstaykov <boycho.staykov@gmail.com>
2015-10-26 13:10:12 +0200

    Load MVC Partials Idea

---
+++ Web/WebBaseSystem.Web/Scripts/Angular/app/controllers/TestHistoryCtrl.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+webBaseModule.controller('TestHistoryCtrl',
+    ['$scope', '$routeParams', 'testHistoryService', '$log',
+    function TestHistoryCtrl($scope, $routeParams, testHistoryService, $log) {
+        
+        function testJQuery(url) {
+            $('#testId').load(url);
+        }
+        $scope.testJQuery = function (url) {
+            testJQuery(url);
+        }
+
+
+        function testHistory(url) {
+            testHistoryService.testHistory(url)
+                            .then(function (data) {
+                                $('#testId').html(data);
+                            })
+                            .catch($log.error);
+        }
+
+        $scope.testHistory = function (url) {
+            testHistory(url);
+        }
+
+        $scope.url;
+        $scope.controllerName = $routeParams.controllerName;
+        $scope.actionName = $routeParams.actionName;
+        if ($scope.controllerName && $scope.actionName) {
+            $scope.url = $scope.controllerName + '/' + $scope.actionName;
+            testHistory($scope.url);
+        }
+    }]
+);
